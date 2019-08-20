ab616a1a8e7f3595ed5df8777f9ec53efe6c2f22
whoops <whoops_mqq@163.com>
2019-08-20 17:38:42 +0800

    2019/08/20-2

--- Geteway/src/main/java/com/whoops/cloud/Gateway.java
+++ Geteway/src/main/java/com/whoops/cloud/Gateway.java
@@ -2,10 +2,23 @@ package com.whoops.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Gateway {
+
     public static void main(String[] args){
         SpringApplication.run(Gateway.class,args);
+    }
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(r -> r.path("/hello")
+                        .uri("lb://CONSUMER")
+                        .order(1)
+                ).build();
     }
 }
